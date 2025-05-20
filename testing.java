24d685fe4e084f4a1096d520668afa899bd70d53
ashutosh229 <akumarjha875@gmail.com>
2025-05-20 20:20:56 +0530

    pushed

---
+++ testing.java
@@ -0,0 +1,8 @@
+public class testing {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test.");
+        System.out.println("Testing, 1, 2, 3...");
+        System.out.println("Goodbye!");
+    }
+}
