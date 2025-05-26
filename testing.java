b7ae7990b247ce9994c675fb99ab21ba1f255aa6
ashutosh229 <akumarjha875@gmail.com>
2025-05-26 16:52:24 +0530

    pushed the reflections

---
+++ testing.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.*;
+
+public class testing {
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = Class.forName("testing");
+        String name = clazz.getName();
+        Method[] methods = clazz.getDeclaredMethods();
+        Method method = methods[0];
+        System.err.println(method.getName());
+        System.err.println(name);
+    }
+
+}
